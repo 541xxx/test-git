9fdcca65a26574c26c781477b78f71bcc60c9c40
haydenwoos <haydenwoos@gmail.com>
2018-05-23 23:31:04 +0800

    first commit

---
+++ src/components/card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p :class="$style.card">
+      {{text}}12312
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['text'],
+};
+</script>
+
+<style  lang='stylus' module>
+.card 
+  padding: 10px;
+  color :red;
+</style>
+
---
+++ vue.config.js
@@ -0,0 +1,16 @@
+// vue.config.js
+const fs = require('fs');
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        data: fs.readFileSync('src/variables.scss', 'utf-8'),
+      },
+    },
+  },
+  loaders: [{
+    test: /\.styl$/,
+    loader: 'css-loader!stylus-loader?paths=node_modules/bootstrap-stylus/stylus/',
+  }],
+};
