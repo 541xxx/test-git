da33950b5c696cd63ba0343a79eccbd1cbf6c9e9
wusiyi <haydenwoos@gmail.com>
2019-04-29 15:31:40 +0800

    feat:

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,14 @@
 const fs = require('fs');
 
 module.exports = {
-  css: {
+  //css: {
     loaderOptions: {
       sass: {
         data: fs.readFileSync('src/variables.scss', 'utf-8'),
-      },
+    //  },
     },
   },
-  loaders: [{
+ // loaders: [{
     test: /\.styl$/,
     loader: 'css-loader!stylus-loader?paths=node_modules/bootstrap-stylus/stylus/',
   }],
